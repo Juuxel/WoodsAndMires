fd3635988b0d8718681fb3fa2c922df54ff653b6
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-03-04 12:54:00 +0200

    Add pine signs

--- src/main/java/juuxel/woodsandmires/block/WamBlocks.java
+++ src/main/java/juuxel/woodsandmires/block/WamBlocks.java
@@ -1,5 +1,9 @@
 package juuxel.woodsandmires.block;
 
+import com.google.common.base.Suppliers;
+import com.terraformersmc.terraform.sign.SpriteIdentifierRegistry;
+import com.terraformersmc.terraform.sign.block.TerraformSignBlock;
+import com.terraformersmc.terraform.sign.block.TerraformWallSignBlock;
 import juuxel.woodsandmires.WoodsAndMires;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -32,10 +36,13 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SignItem;
 import net.minecraft.item.TallBlockItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
 
 public final class WamBlocks {
     public static final Block PINE_LOG = new PillarBlock(copyWoodSettings(Blocks.OAK_LOG));
@@ -47,6 +54,9 @@ public final class WamBlocks {
     public static final Block PINE_FENCE_GATE = new FenceGateBlock(copyWoodSettings(Blocks.OAK_FENCE_GATE));
     public static final Block PINE_BUTTON = new WoodenButtonBlock(copyWoodSettings(Blocks.OAK_BUTTON));
     public static final Block PINE_PRESSURE_PLATE = new PressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, copyWoodSettings(Blocks.OAK_PRESSURE_PLATE));
+    public static final Block PINE_SIGN = new TerraformSignBlock(WoodsAndMires.id("entity/signs/pine"), copyWoodSettings(Blocks.OAK_SIGN));
+    // We have to evaluate this *after* PINE_SIGN has registered due to the loot table condition.
+    public static final Supplier<Block> PINE_WALL_SIGN = Suppliers.memoize(() -> new TerraformWallSignBlock(WoodsAndMires.id("entity/signs/pine"), copyWoodSettings(PINE_SIGN).dropsLike(PINE_SIGN)));
     public static final Block PINE_LEAVES = Blocks.createLeavesBlock(BlockSoundGroup.GRASS);
     public static final Block PINE_SAPLING = new SaplingBlock(new PineSaplingGenerator(), AbstractBlock.Settings.copy(Blocks.OAK_SAPLING));
     public static final Block POTTED_PINE_SAPLING = new FlowerPotBlock(PINE_SAPLING, createFlowerPotSettings());
@@ -79,6 +89,8 @@ public final class WamBlocks {
         register("pine_fence_gate", PINE_FENCE_GATE, ItemGroup.REDSTONE);
         register("pine_button", PINE_BUTTON, ItemGroup.REDSTONE);
         register("pine_pressure_plate", PINE_PRESSURE_PLATE, ItemGroup.REDSTONE);
+        register("pine_sign", PINE_SIGN, () -> new SignItem(new Item.Settings().maxCount(16).group(ItemGroup.DECORATIONS), PINE_SIGN, PINE_WALL_SIGN.get()));
+        register("pine_wall_sign", PINE_WALL_SIGN.get(), (Item) null);
         register("pine_leaves", PINE_LEAVES, ItemGroup.DECORATIONS);
         register("pine_sapling", PINE_SAPLING, ItemGroup.DECORATIONS);
         register("potted_pine_sapling", POTTED_PINE_SAPLING, (Item) null);
@@ -180,6 +192,15 @@ public final class WamBlocks {
     private static void register(String id, Block block, @Nullable Item item) {
         Registry.register(Registry.BLOCK, WoodsAndMires.id(id), block);
 
+        if (item != null) {
+            Registry.register(Registry.ITEM, WoodsAndMires.id(id), item);
+        }
+    }
+
+    private static void register(String id, Block block, Supplier<@Nullable Item> itemSupplier) {
+        Registry.register(Registry.BLOCK, WoodsAndMires.id(id), block);
+
+        @Nullable Item item = itemSupplier.get();
         if (item != null) {
             Registry.register(Registry.ITEM, WoodsAndMires.id(id), item);
         }
