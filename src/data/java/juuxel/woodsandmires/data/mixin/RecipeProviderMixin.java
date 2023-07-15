1cbccb9a70e4837f7f6ce191f385b1a39d815f8d
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-07-15 22:58:39 +0300

    Fix vanilla sticks sneaking in instead of c:wooden_rods

--- src/data/java/juuxel/woodsandmires/data/mixin/RecipeProviderMixin.java
+++ src/data/java/juuxel/woodsandmires/data/mixin/RecipeProviderMixin.java
@@ -1,9 +1,14 @@
 package juuxel.woodsandmires.data.mixin;
 
+import juuxel.woodsandmires.data.builtin.CommonItemTags;
 import net.minecraft.data.server.RecipeProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RecipeProvider.class)
 abstract class RecipeProviderMixin {
@@ -14,5 +19,16 @@ abstract class RecipeProviderMixin {
     )
     private static String addNewLine(String json) {
         return json + "\n";
+    }
+
+    @Redirect(
+        method = {"createFenceRecipe", "createFenceGateRecipe", "createSignRecipe"},
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/data/server/recipe/ShapedRecipeJsonBuilder;input(Ljava/lang/Character;Lnet/minecraft/item/ItemConvertible;)Lnet/minecraft/data/server/recipe/ShapedRecipeJsonBuilder;")
+    )
+    private static ShapedRecipeJsonBuilder replaceStick(ShapedRecipeJsonBuilder builder, Character c, ItemConvertible item) {
+        if (item.asItem() == Items.STICK) {
+            return builder.input(c, CommonItemTags.WOODEN_RODS);
+        }
+        return builder.input(c, item);
     }
 }
